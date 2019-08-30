e10d03748e13bbcd50780125b0258512d1c4b234
Aturs85 <arturs.ardavs@gmail.com>
2019-08-30 15:14:25 +0100

    ranging and messaging working, to be tested on two devices

--- uwbmsglistener.cpp
+++ uwbmsglistener.cpp
@@ -90,7 +90,7 @@ static uint32 status_reg = 0;
 /* This is the delay from the end of the frame transmission to the enable of the receiver, as programmed for the DW1000's wait for response feature. */
 #define RESP_TX_TO_FINAL_RX_DLY_UUS 700//500
 /* Receive final timeout. See NOTE 5 below. */
-#define FINAL_RX_TIMEOUT_UUS 5000//3300
+#define FINAL_RX_TIMEOUT_UUS 7000//3300
 /* Preamble timeout, in multiple of PAC size. See NOTE 6 below. */
 #define PRE_TIMEOUT 128
 
